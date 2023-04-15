3b6639efa6d40f00eab6341b6400df099c4423b7
kmomozin <khinmomozin@gmail.com>
2023-04-15 13:27:08 -0600

    initial about page setup

--- mo-portfolio/components/About.js
+++ mo-portfolio/components/About.js
@@ -1,7 +1,16 @@
 const About = () => {
   return (
     <section className="about-section light-background">
-      <h1>This is about me</h1>
+      <h3>Khin Mo Mo Zin</h3>
+      <p>
+        Mo Mo is a Calgary-based software engineer with prior experience
+        in software development. Her focus on the mojo project was to
+        provide users with the monthly calendar view, journal entry,
+        and, checklist customization features. When she’s not busy
+        challenging herself with new coding techniques, she can be found
+        hiking in the Canadian Rockies or knitting at home with her cat,
+        Chia.
+      </p>
     </section>
   );
 };
