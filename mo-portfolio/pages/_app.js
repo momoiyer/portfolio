b9318399b69fbe6577efc4657b4a500e504fd2ad
kmomozin <khinmomozin@gmail.com>
2023-04-16 16:54:16 -0600

    add bootstrap and carousel

--- mo-portfolio/pages/_app.js
+++ mo-portfolio/pages/_app.js
@@ -1,11 +1,15 @@
 import Head from 'next/head';
+// //add MDBootstrap
+// import * as mdb from 'mdb-ui-kit'; // lib
+// import { Input } from 'mdb-ui-kit'; // module
+// @import '~mdb-ui-kit/css/mdb.min.css';
 //add custom global css
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (<>
     <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
     </Head>
     <Component {...pageProps} />
   </>
