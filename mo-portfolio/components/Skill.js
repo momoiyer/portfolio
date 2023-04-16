b0be8ac82c3228cf350ef1af798d4862fb040433
kmomozin <khinmomozin@gmail.com>
2023-04-16 16:03:05 -0600

    update skill sectiona and colors

--- mo-portfolio/components/Skill.js
+++ mo-portfolio/components/Skill.js
@@ -4,11 +4,11 @@ const Skill = () => {
   return (
     <section className="skill-section  dark-background">
       <h1>Explore my skills</h1>
-      <p>languages</p>
+      <p>Languages</p>
       <div className='skill-section__languages'>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/js.svg"
+            src="/skill-logo/js.svg"
             alt="Javascript Log"
             width={30}
             height={30} />
@@ -16,7 +16,7 @@ const Skill = () => {
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/ruby.svg"
+            src="/skill-logo/ruby.svg"
             alt="Ruby Logo"
             width={30}
             height={30} />
@@ -24,7 +24,7 @@ const Skill = () => {
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/css.svg"
+            src="/skill-logo/css.svg"
             alt="Css Logo"
             width={30}
             height={30} />
@@ -32,7 +32,7 @@ const Skill = () => {
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/java.svg"
+            src="/skill-logo/java.svg"
             alt="Java Logo"
             width={30}
             height={30} />
@@ -40,7 +40,7 @@ const Skill = () => {
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/android.svg"
+            src="/skill-logo/android.svg"
             alt="Android Logo"
             width={30}
             height={30} />
@@ -48,7 +48,7 @@ const Skill = () => {
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/csharp.svg"
+            src="/skill-logo/csharp.svg"
             alt="C# Logo"
             width={30}
             height={30} />
@@ -56,7 +56,7 @@ const Skill = () => {
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/html5.svg"
+            src="/skill-logo/html5.svg"
             alt="HTML Logo"
             width={30}
             height={30} />
@@ -64,7 +64,7 @@ const Skill = () => {
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/php.svg"
+            src="/skill-logo/php.svg"
             alt="PHP Logo"
             width={30}
             height={30} />
@@ -72,278 +72,197 @@ const Skill = () => {
         </div>
       </div>
 
-      <p>farmeworks</p>
+      <p>Frameworks & Libriries</p>
       <div className='skill-section__languages'>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/js.svg"
-            alt="Javascript Log"
+            src="/skill-logo/react.svg"
+            alt="React Logo"
             width={30}
             height={30} />
-          <span className='language-desc'>Javascript</span>
+          <span className='language-desc'>React</span>
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/ruby.svg"
-            alt="Ruby Logo"
+            src="/skill-logo/node.svg"
+            alt="Node.js Logo"
             width={30}
             height={30} />
-          <span className='language-desc'>Ruby</span>
+          <span className='language-desc'>Node.js</span>
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/css.svg"
-            alt="Css Logo"
+            src="/skill-logo/rails.svg"
+            alt="Rails Logo"
             width={30}
             height={30} />
-          <span className='language-desc'>CSS</span>
+          <span className='language-desc'>Rails</span>
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/java.svg"
-            alt="Java Logo"
+            src="/skill-logo/jquery.svg"
+            alt="JQuery Logo"
             width={30}
             height={30} />
-          <span className='language-desc'>Java</span>
+          <span className='language-desc'>JQuery</span>
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/android.svg"
-            alt="Android Logo"
+            src="/skill-logo/express.svg"
+            alt="Express Logo"
             width={30}
             height={30} />
-          <span className='language-desc'>Android</span>
+          <span className='language-desc'>Express</span>
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/csharp.svg"
-            alt="C# Logo"
+            src="/skill-logo/sass.svg"
+            alt="SASS Logo"
             width={30}
             height={30} />
-          <span className='language-desc'>C#</span>
+          <span className='language-desc'>SASS</span>
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/html5.svg"
-            alt="HTML Logo"
+            src="/skill-logo/bootstrap.png"
+            alt="Bootstrap Logo"
             width={30}
             height={30} />
-          <span className='language-desc'>HTML</span>
-        </div>
-        <div className='skill-section__languages-group'>
-          <Image
-            src="/language-logo/php.svg"
-            alt="PHP Logo"
-            width={30}
-            height={30} />
-          <span className='language-desc'>PHP</span>
+          <span className='language-desc'>Bootstrap</span>
         </div>
       </div>
 
-      <p>testing</p>
+      <p>Database & Systems</p>
       <div className='skill-section__languages'>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/js.svg"
-            alt="Javascript Log"
+            src="/skill-logo/mysql.svg"
+            alt="MySQL Log"
             width={30}
             height={30} />
-          <span className='language-desc'>Javascript</span>
+          <span className='language-desc'>MySQL</span>
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/ruby.svg"
-            alt="Ruby Logo"
+            src="/skill-logo/postgres.svg"
+            alt="Postgres Logo"
             width={30}
             height={30} />
-          <span className='language-desc'>Ruby</span>
+          <span className='language-desc'>Postgres</span>
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/css.svg"
-            alt="Css Logo"
+            src="/skill-logo/mongodb.png"
+            alt="MongoDB Logo"
             width={30}
             height={30} />
-          <span className='language-desc'>CSS</span>
+          <span className='language-desc'>MongoDB</span>
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/java.svg"
-            alt="Java Logo"
+            src="/skill-logo/sqlite.svg"
+            alt="SQLite Logo"
             width={30}
             height={30} />
-          <span className='language-desc'>Java</span>
+          <span className='language-desc'>SQLite</span>
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/android.svg"
-            alt="Android Logo"
+            src="/skill-logo/graphql.svg"
+            alt="GraphQL Logo"
             width={30}
             height={30} />
-          <span className='language-desc'>Android</span>
+          <span className='language-desc'>GraphQL</span>
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/csharp.svg"
-            alt="C# Logo"
-            width={30}
+            src="/skill-logo/sybase.png"
+            alt="SQL Anywhere Logo"
+            width={40}
             height={30} />
-          <span className='language-desc'>C#</span>
+          <span className='language-desc'>SQL Anywhere</span>
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/html5.svg"
-            alt="HTML Logo"
+            src="/skill-logo/firebase.svg"
+            alt="Firebase Logo"
             width={30}
             height={30} />
-          <span className='language-desc'>HTML</span>
-        </div>
-        <div className='skill-section__languages-group'>
-          <Image
-            src="/language-logo/php.svg"
-            alt="PHP Logo"
-            width={30}
-            height={30} />
-          <span className='language-desc'>PHP</span>
+          <span className='language-desc'>Firebase</span>
         </div>
       </div>
 
-
-      <p>database</p>
+      <p>Testing</p>
       <div className='skill-section__languages'>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/js.svg"
-            alt="Javascript Log"
+            src="/skill-logo/mocha.png"
+            alt="Mocha Log"
             width={30}
             height={30} />
-          <span className='language-desc'>Javascript</span>
+          <span className='language-desc'>Mocha</span>
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/ruby.svg"
-            alt="Ruby Logo"
+            src="/skill-logo/chai.svg"
+            alt="Chai Logo"
             width={30}
             height={30} />
-          <span className='language-desc'>Ruby</span>
+          <span className='language-desc'>Chai</span>
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/css.svg"
-            alt="Css Logo"
+            src="/skill-logo/jest.svg"
+            alt="Jest Logo"
             width={30}
             height={30} />
-          <span className='language-desc'>CSS</span>
+          <span className='language-desc'>Jest</span>
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/java.svg"
-            alt="Java Logo"
+            src="/skill-logo/cypress.png"
+            alt="Cypress Logo"
             width={30}
             height={30} />
-          <span className='language-desc'>Java</span>
+          <span className='language-desc'>Cypress</span>
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/android.svg"
-            alt="Android Logo"
+            src="/skill-logo/storybook.svg"
+            alt="Storybook Logo"
             width={30}
             height={30} />
-          <span className='language-desc'>Android</span>
+          <span className='language-desc'>Storybook</span>
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/csharp.svg"
-            alt="C# Logo"
+            src="/skill-logo/rspec.svg"
+            alt="Rspec Logo"
             width={30}
             height={30} />
-          <span className='language-desc'>C#</span>
-        </div>
-        <div className='skill-section__languages-group'>
-          <Image
-            src="/language-logo/html5.svg"
-            alt="HTML Logo"
-            width={30}
-            height={30} />
-          <span className='language-desc'>HTML</span>
-        </div>
-        <div className='skill-section__languages-group'>
-          <Image
-            src="/language-logo/php.svg"
-            alt="PHP Logo"
-            width={30}
-            height={30} />
-          <span className='language-desc'>PHP</span>
+          <span className='language-desc'>Rspec</span>
         </div>
       </div>
 
-
-      <p>CDM</p>
+      <p>CMS</p>
       <div className='skill-section__languages'>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/js.svg"
-            alt="Javascript Log"
+            src="/skill-logo/git.svg"
+            alt="Git Log"
             width={30}
             height={30} />
-          <span className='language-desc'>Javascript</span>
+          <span className='language-desc'>Git</span>
         </div>
         <div className='skill-section__languages-group'>
           <Image
-            src="/language-logo/ruby.svg"
-            alt="Ruby Logo"
+            src="/skill-logo/svn.png"
+            alt="SVN Logo"
             width={30}
             height={30} />
-          <span className='language-desc'>Ruby</span>
+          <span className='language-desc'>SVN</span>
         </div>
-        <div className='skill-section__languages-group'>
-          <Image
-            src="/language-logo/css.svg"
-            alt="Css Logo"
-            width={30}
-            height={30} />
-          <span className='language-desc'>CSS</span>
-        </div>
-        <div className='skill-section__languages-group'>
-          <Image
-            src="/language-logo/java.svg"
-            alt="Java Logo"
-            width={30}
-            height={30} />
-          <span className='language-desc'>Java</span>
-        </div>
-        <div className='skill-section__languages-group'>
-          <Image
-            src="/language-logo/android.svg"
-            alt="Android Logo"
-            width={30}
-            height={30} />
-          <span className='language-desc'>Android</span>
-        </div>
-        <div className='skill-section__languages-group'>
-          <Image
-            src="/language-logo/csharp.svg"
-            alt="C# Logo"
-            width={30}
-            height={30} />
-          <span className='language-desc'>C#</span>
-        </div>
-        <div className='skill-section__languages-group'>
-          <Image
-            src="/language-logo/html5.svg"
-            alt="HTML Logo"
-            width={30}
-            height={30} />
-          <span className='language-desc'>HTML</span>
-        </div>
-        <div className='skill-section__languages-group'>
-          <Image
-            src="/language-logo/php.svg"
-            alt="PHP Logo"
-            width={30}
-            height={30} />
-          <span className='language-desc'>PHP</span>
-        </div>
+
       </div>
 
     </section>
