a0425ab315f6d27d1b281f955bb465abfdaced62
kmomozin <khinmomozin@gmail.com>
2023-04-16 19:41:15 -0600

    remove background from carousel

--- mo-portfolio/components/Project.js
+++ mo-portfolio/components/Project.js
@@ -30,6 +30,12 @@ const Project = () => {
               data-mdb-slide-to="2"
               aria-label="Slide 3"
             ></button>
+            <button
+              type="button"
+              data-mdb-target="#carouselBasicExample"
+              data-mdb-slide-to="3"
+              aria-label="Slide 4"
+            ></button>
           </div>
 
           {/* <!-- Inner --> */}
@@ -46,13 +52,12 @@ const Project = () => {
             </div>
 
             {/* <!-- Single item --> */}
-            <div className="carousel-item" >
-              <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp" className="d-block w-100" alt="Canyon at Nigh" />
-              {/* <div className="carousel-caption d-none d-md-block"> */}
+            <div className="carousel-item" >{/* <div className="carousel-caption d-none d-md-block"> */}
               <div className="carousel-image-description" >
                 <h5>Second slide label</h5>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
               </div>
+              <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp" className="d-block w-100" alt="Canyon at Nigh" />
             </div>
 
             {/* <!-- Single item --> */}
@@ -64,6 +69,15 @@ const Project = () => {
                 <h5>Third slide label</h5>
                 <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
               </div>
+            </div>
+
+            {/* <!-- Single item --> */}
+            <div className="carousel-item" >{/* <div className="carousel-caption d-none d-md-block"> */}
+              <div className="carousel-image-description" >
+                <h5>Second slide label</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </div>
+              <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp" className="d-block w-100" alt="Canyon at Nigh" />
             </div>
           </div>
           {/* <!-- Inner -->
