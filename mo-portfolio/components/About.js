fa3fdea6a0b62326166d11969593790e9a8fede1
kmomozin <khinmomozin@gmail.com>
2023-04-20 18:16:49 -0600

    remove dash line from journey

--- mo-portfolio/components/About.js
+++ mo-portfolio/components/About.js
@@ -2,14 +2,14 @@ import Image from 'next/image';
 
 const About = () => {
   return (
-    <section className="about-section light-background" data-aos="zoom-in">
-      <div className='about-section__title'>
+    <section className="about-section light-background" >
+      <div className='about-section__title' data-aos="zoom-in">
         <h1>My Journey</h1>
         <p>Summary of my life, education and professional experience</p>
       </div>
 
       <div className='about-section__item_list'>
-        <div className='about-section__item'>
+        <div className='about-section__item' data-aos="fade-down">
           <div className='about-section__item-background'>
             <div className='about-section__item-image'>
               <Image
@@ -25,7 +25,7 @@ const About = () => {
             </div>
           </div>
         </div>
-        <div className='about-section__item about-section__item-left'>
+        <div className='about-section__item about-section__item-left' data-aos="fade-down">
           <div className='about-section__item-background'>
             <div className='about-section__item-image'>
               <Image
@@ -41,12 +41,12 @@ const About = () => {
             </div>
             {/* <p>2009-2017</p> */}
             <div className='about-section__item-desc'>
-              <p>Migrated to Singapore. Graudated with BSc. in IT & Software Engineering. Worked in IBM Datacenter supporting & supervising the IT Operation Team for 4 years</p>
+              <p>Migrated to Singapore. Graudated with BSc. in Information System & Software Engineering. Worked at IBM Datacenter supporting & supervising the IT Operation Team for 4 years</p>
             </div>
 
           </div>
         </div>
-        <div className='about-section__item'>
+        <div className='about-section__item' data-aos="fade-down">
           <div className='about-section__item-background'>
             <div className='about-section__item-image'>
               <Image
@@ -62,11 +62,11 @@ const About = () => {
             </div>
             {/* <p>2017-2020</p> */}
             <div className='about-section__item-desc'>
-              <p>Started missing coding and went back to school to get Graduate Diploma in Systems Analysis. Got the first full stack developer job that brought so much exciement every day for 3 years!</p>
+              <p>Started missing coding and went back to school to get Graduate Diploma in Systems Analysis. Got the first developer job (Full Stack) that brought so much exciement every single day for 3 years!</p>
             </div>
           </div>
         </div>
-        <div className='about-section__item about-section__item-left'>
+        <div className='about-section__item about-section__item-left' data-aos="fade-down">
           <div className='about-section__item-background'> <div className='about-section__item-image'>
             <Image
               src="/about-me/canada.png"
@@ -81,11 +81,11 @@ const About = () => {
           </div>
             {/* <p>2020-2023</p> */}
             <div className='about-section__item-desc'>
-              <p>Finally migrated to my dream country! Taking advantage of new found freedom, started exploring the entrepreneur side of me and started an E-commerce business with Amazon. Achieved high customer rating and profit margin.</p>
+              <p>Finally migrated to my dream country, Canada! Taking advantage of new found freedom, started exploring the entrepreneur side of me and started an E-commerce business with Amazon. Achieved high customer rating and profit margin.</p>
             </div>
           </div>
         </div>
-        <div className='about-section__item'>
+        <div className='about-section__item' data-aos="fade-down">
           <div className='about-section__item-background'>
             <div className='about-section__item-image'>
               <Image
@@ -101,20 +101,20 @@ const About = () => {
             </div>
             {/* <p>2013-present</p> */}
             <div className='about-section__item-desc'>
-              <p>After 3 years, started missing my true passion of programming and joined bootcamp to modernize my tech stack. Currently volunteering at Calgary startup as a full stack developer and team lead of the front-end team.</p>
+              <p>After almost 3 years, started missing my true passion of programming and joined bootcamp to modernize my tech stack. Currently volunteering at Calgary startup as a full stack developer and team lead of the front-end team.</p>
             </div>
           </div>
         </div>
       </div>
-
-      <div className='dash-1'>
+      {/* 
+      <div className='dash-1' data-aos="fade-down">
         <Image
           src="/about-me/dash3.png"
           alt="start of journey"
           width={150}
           height={180} />
       </div>
-      <div className='dash-2'>
+      <div className='dash-2' data-aos="fade-down">
         <Image
           src="/about-me/dash2.png"
           alt="start of journey"
@@ -122,114 +122,21 @@ const About = () => {
           height={160} />
       </div>
 
-      <div className='dash-3'>
+      <div className='dash-3' data-aos="fade-down">
         <Image
           src="/about-me/dash1.png"
           alt="start of journey"
           width={180}
           height={200} />
       </div>
-      <div className='dash-4'>
+      <div className='dash-4' data-aos="fade-down">
         <Image
           src="/about-me/dash2.png"
           alt="start of journey"
           width={160}
           height={160} />
-      </div>
-
-      {/* <Image
-        src="/about-me/frame.png"
-        alt="start of journey"
-        width={420}
-        height={120} /> */}
-      {/* <div>
-        <Image
-          src="/about-me/start.png"
-          alt="start of journey"
-          width={70}
-          height={70} />
-        <p>Born in Myanmar and been dreaming to migrate to Canada since after watching The Wolverine Origin </p>
-      </div>
-      <div>
-        <Image
-          src="/about-me/moving.png"
-          alt="move to singapore"
-          width={70}
-          height={70} />
-        <p>Migrated to Singapore to continue University (never spoken in English in my life before) </p>
-      </div>
-      <div>
-        <Image
-          src="/about-me/graduated.png"
-          alt="graduated"
-          width={70}
-          height={70} />
-        <p>Graduated with Bachelor in Information System and Software Engineering </p>
-      </div>
-      <div>
-        <Image
-          src="/about-me/ibm.png"
-          alt="first job"
-          width={70}
-          height={70} />
-        <p>Worked in IBM Datacenter supporting the IT Operation Team for 4 years  </p>
-      </div>
-      <div>
-        <Image
-          src="/about-me/student.png"
-          alt="system analyst"
-          width={70}
-          height={70} />
-        <p>Started missing coding and back to school to get Graduate Diploma in Systems Analysis</p>
-      </div>
-      <div>
-        <Image
-          src="/about-me/developer.png"
-          alt="developer"
-          width={70}
-          height={70} />
-        <p>First development job that brought so much exciement every single day for 3 years! </p>
-      </div>
-      <div>
-        <Image
-          src="/about-me/canada.png"
-          alt="migrate to canada"
-          width={70}
-          height={70} />
-        <p>Finally migrated to my dream country! </p>
-      </div>
-      <div>
-        <Image
-          src="/about-me/idea.png"
-          alt="entrepreneur journey"
-          width={70}
-          height={70} />
-        <p>Taking advantage of new found freedom, started exploring the entrepreneur side of me </p>
-      </div>
-      <div>
-        <Image
-          src="/about-me/amazon.png"
-          alt="Amazon journey"
-          width={70}
-          height={70} />
-        <p>Started an E-commerce family startup business with Amazon and achieve high customer rating and profit margin </p>
-      </div>
-      <div>
-        <Image
-          src="/about-me/lighthouselabs.jpg"
-          alt="LHL journey"
-          width={70}
-          height={70} />
-        <p>After 3 years, started missing my true passion of programming and joined LHL to modernize my tech stack</p>
-      </div>
-      <div>
-        <Image
-          src="/about-me/volunteer.png"
-          alt="Volunteer journey"
-          width={70}
-          height={70} />
-        <p>Currently volunteering at local startup as a full stack developer and team lead of the front-end web team</p>
       </div> */}
+
     </section >
   );
 };
