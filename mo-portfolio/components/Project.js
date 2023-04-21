00d6095e3ea3b00a45e8157b61fa6389762ab61e
kmomozin <khinmomozin@gmail.com>
2023-04-21 11:43:29 -0600

    modify mojo project detail section

--- mo-portfolio/components/Project.js
+++ mo-portfolio/components/Project.js
@@ -169,7 +169,7 @@ const Project = () => {
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
-              <h5 className="modal-title" id="project1DetailsLabel">Project Details</h5>
+              <h2 className="modal-title" id="project1DetailsLabel">Project Details</h2>
               <button type="button" className="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
@@ -177,56 +177,83 @@ const Project = () => {
               <div className='modal-body__tech-stack'>
                 <p>Tech Stack: </p>
                 <div>
-                  <Image
-                    src="/skill-logo/node.svg"
-                    alt="Node.js Logo"
-                    width={50}
-                    height={30} />
-                  <Image
-                    src="/skill-logo/express.svg"
-                    alt="Express Logo"
-                    width={30}
-                    height={30} />
-                  <Image
-                    src="/skill-logo/react.svg"
-                    alt="React Logo"
-                    width={30}
-                    height={30} />
-                  <Image
-                    src="/skill-logo/sass.svg"
-                    alt="SASS Logo"
-                    width={30}
-                    height={30} />
-                  <Image
-                    src="/skill-logo/postgres.svg"
-                    alt="Postgres Logo"
-                    width={30}
-                    height={30} />
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/nodejs.png"
+                      alt="Node.js Logo"
+                      width={50}
+                      height={30} />
+                    <span className='language-desc'>Node.js</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/skill-logo/express.svg"
+                      alt="Express Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>Express</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/react.png"
+                      alt="React Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>React</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/sass.svg"
+                      alt="SASS Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>SASS</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/postgres.png"
+                      alt="Postgres Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>Postgres</span>
+                  </div>
                 </div>
               </div>
               <div className='modal-body__api-lib'>
                 <p>API & Libraries: </p>
                 <div>
-                  <Image
-                    src="/colored-logo/fullcalendar.png"
-                    alt="React Full Calendar Logo"
-                    width={40}
-                    height={40} />
-                  <Image
-                    src="/colored-logo/draftjs.svg"
-                    alt="Draft.js Logo"
-                    width={30}
-                    height={30} />
-                  <Image
-                    src="/colored-logo/spotify.png"
-                    alt="Draft.js Logo"
-                    width={30}
-                    height={30} />
-                  <Image
-                    src="/colored-logo/weather.png"
-                    alt="Draft.js Logo"
-                    width={60}
-                    height={30} />
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/fullcalendar.png"
+                      alt="React Full Calendar Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>React Fullcalendar</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/draftjs.svg"
+                      alt="Draft.js Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>Darft.js</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/spotify.png"
+                      alt="Draft.js Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>Spotify API</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/weather.png"
+                      alt="Draft.js Logo"
+                      width={60}
+                      height={30} />
+                    <span className='language-desc'>Weather API</span>
+                  </div>
                 </div>
               </div>
               <div className='modal-body__source-code'>
@@ -238,9 +265,9 @@ const Project = () => {
                 </Link>
               </div>
             </div>
-            <div className="modal-footer">
+            {/* <div className="modal-footer">
               <button type="button" className="modal-button" data-mdb-dismiss="modal">Close</button>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
