7a277d4ad193cd986e47935d6dad85031908e38d
kmomozin <khinmomozin@gmail.com>
2023-04-24 15:49:22 -0600

    wikimap detail setup

--- mo-portfolio/components/Project.js
+++ mo-portfolio/components/Project.js
@@ -1,4 +1,5 @@
-import Modal from "./Modal.js";
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Project = () => {
   return (
@@ -10,7 +11,7 @@ const Project = () => {
 
       <div className="project-section__carousel" data-aos="flip-left">
         {/* <!-- Carousel wrapper --> */}
-        <div id="project-carousel" className="carousel slide carousel-fade" data-mdb-ride="carousel">
+        <div id="project-carousel" className="carousel slide carousel-fade" >
           {/* <!-- Indicators --> */}
           <div className="carousel-indicators">
             <button
@@ -56,31 +57,20 @@ const Project = () => {
           {/* <!-- Inner --> */}
           <div className="carousel-inner ">
             {/* <!-- Single item --> */}
-            <div className="carousel-item active" data-mdb-interval="500000">
-              <img src="/projects/mojo/mojo-v3.png" alt="Sunset Over the City" />
-              {/* <div className="carousel-caption d-none d-md-block"> */}
+            <div className="carousel-item active" >
+              <img src="/projects/mojo/mojo.png" alt="Sunset Over the City" />
 
               <div className="carousel-image-description" >
-                {/* <div>
-                  <h2>MoJo</h2>
-                  <p>Mood Management Web App</p>
-                  <p>March 2023</p>
-                </div> */}
                 <button className='carousel-item__button' data-mdb-toggle="modal" data-mdb-target="#project1Details">Click for full portfolio</button>
               </div>
             </div>
 
             {/* <!-- Single item --> */}
-            <div className="carousel-item" >{/* <div className="carousel-caption d-none d-md-block"> */}
-              <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp" className="d-block w-100" alt="Canyon at Nigh" data-aos="fade-left" />
+            <div className="carousel-item" >
+              <img src="/projects/wikimap/wikimap.png" alt="Wikimap" />
 
               <div className="carousel-image-description" >
-                <div>
-                  <h2>Wiki Map</h2>
-                  <p>Customized Maps with collection of points of interest</p>
-                  <p>Feb 2023</p>
-                </div>
-                <button className='carousel-item__button'>Click for full portfolio</button>
+                <button className='carousel-item__button' data-mdb-toggle="modal" data-mdb-target="#project2Details">Click for full portfolio</button>
               </div>
             </div>
 
@@ -99,7 +89,7 @@ const Project = () => {
             </div>
 
             {/* <!-- Single item --> */}
-            <div className="carousel-item" >{/* <div className="carousel-caption d-none d-md-block"> */}
+            <div className="carousel-item" >
 
               <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp" className="d-block w-100" alt="Canyon at Nigh" />
 
@@ -114,7 +104,7 @@ const Project = () => {
             </div>
 
             {/* <!-- Single item --> */}
-            <div className="carousel-item" >{/* <div className="carousel-caption d-none d-md-block"> */}
+            <div className="carousel-item" >
 
               <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp" className="d-block w-100" alt="Canyon at Nigh" />
 
@@ -129,7 +119,7 @@ const Project = () => {
             </div>
 
             {/* <!-- Single item --> */}
-            <div className="carousel-item" >{/* <div className="carousel-caption d-none d-md-block"> */}
+            <div className="carousel-item" >
 
               <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp" className="d-block w-100" alt="Canyon at Nigh" />
 
@@ -159,8 +149,219 @@ const Project = () => {
         {/* <!-- Carousel wrapper --> */}
       </div>
 
-      <Modal />
 
+
+
+      {/* <!-- MoJo Modal --> */}
+      <div className="modal fade" id="project1Details" tabindex="-1" aria-labelledby="project1DetailsLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h2 className="modal-title" id="project1DetailsLabel">Project Details</h2>
+              <button type="button" className="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <p className='modal-body__desc'>A mood management web app for daily journaling, habit tracking, and music suggestion to match the current mood.</p>
+              <div className='modal-body__tech-stack'>
+                <p>Tech Stack: </p>
+                <div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/nodejs.png"
+                      alt="Node.js Logo"
+                      width={50}
+                      height={30} />
+                    <span className='language-desc'>Node.js</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/skill-logo/express.svg"
+                      alt="Express Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>Express</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/react.png"
+                      alt="React Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>React</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/sass.svg"
+                      alt="SASS Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>SASS</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/postgres.png"
+                      alt="Postgres Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>Postgres</span>
+                  </div>
+                </div>
+              </div>
+              <div className='modal-body__api-lib'>
+                <p>API & Libraries: </p>
+                <div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/fullcalendar.png"
+                      alt="React Full Calendar Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>React Fullcalendar</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/draftjs.svg"
+                      alt="Draft.js Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>Darft.js</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/spotify.png"
+                      alt="Draft.js Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>Spotify API</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/weather.png"
+                      alt="Draft.js Logo"
+                      width={60}
+                      height={30} />
+                    <span className='language-desc'>Weather API</span>
+                  </div>
+                </div>
+              </div>
+              <div className='modal-body__source-code'>
+                <Link href="https://github.com/MoMoZin/MoJo" legacyBehavior>
+                  <a target="_blank" className='modal-body__source-code___git'>
+                    <i className="fa-brands fa-github fa-lg"></i>
+                    <span>View Source Code</span>
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      {/* <!-- WikiMap Modal --> */}
+      <div className="modal fade" id="project2Details" tabindex="-1" aria-labelledby="project2DetailsLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h2 className="modal-title" id="project2DetailsLabel">Project Details</h2>
+              <button type="button" className="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <p className='modal-body__desc'>A responsive web app that allows users to collaboratively create maps which list multiple "points".</p>
+              <div className='modal-body__tech-stack'>
+                <p>Tech Stack: </p>
+                <div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/nodejs.png"
+                      alt="Node.js Logo"
+                      width={50}
+                      height={30} />
+                    <span className='language-desc'>Node.js</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/skill-logo/express.svg"
+                      alt="Express Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>Express</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/react.png"
+                      alt="React Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>React</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/sass.svg"
+                      alt="SASS Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>SASS</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/postgres.png"
+                      alt="Postgres Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>Postgres</span>
+                  </div>
+                </div>
+              </div>
+              <div className='modal-body__api-lib'>
+                <p>API & Libraries: </p>
+                <div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/fullcalendar.png"
+                      alt="React Full Calendar Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>React Fullcalendar</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/draftjs.svg"
+                      alt="Draft.js Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>Darft.js</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/spotify.png"
+                      alt="Draft.js Logo"
+                      width={30}
+                      height={30} />
+                    <span className='language-desc'>Spotify API</span>
+                  </div>
+                  <div className='skill-section__languages-group'>
+                    <Image
+                      src="/colored-logo/weather.png"
+                      alt="Draft.js Logo"
+                      width={60}
+                      height={30} />
+                    <span className='language-desc'>Weather API</span>
+                  </div>
+                </div>
+              </div>
+              <div className='modal-body__source-code'>
+                <Link href="https://github.com/MoMoZin/MoJo" legacyBehavior>
+                  <a target="_blank" className='modal-body__source-code___git'>
+                    <i className="fa-brands fa-github fa-lg"></i>
+                    <span>View Source Code</span>
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
 
     </section >
