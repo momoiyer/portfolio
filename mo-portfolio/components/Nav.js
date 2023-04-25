22d5e6fc7bc21f97f7c52ebeecf7cf5a4d86d2f8
kmomozin <khinmomozin@gmail.com>
2023-04-25 15:51:54 -0600

    make nav bar fully responsivce

--- mo-portfolio/components/Nav.js
+++ mo-portfolio/components/Nav.js
@@ -4,26 +4,14 @@ import React, { useState } from "react";
 const Nav = () => {
 
   const [navActive, setNavActive] = useState(false);
-  // const [activeIdx, setActiveIdx] = useState(-1);
 
   return (
     <nav className='nav-bar'>
       <span><Link href={'#intro'} className='nav-bar__name-logo'>Khin Mo Mo Zin</Link></span>
-      {/* <ul>
-        <li><Link className='nav-bar__item' href={'#about'}>about</Link></li>
-        <li><Link className='nav-bar__item' href={'#skills'}>skills</Link></li>
-        <li><Link className='nav-bar__item' href={'#projects'}>projects</Link></li>
-        <li><Link className='nav-bar__item' href={'#blogs'}>blogs</Link></li>
-        <li><Link className='nav-bar__item' href={'#contact'}>contact</Link></li>
-        <li><a className='nav-bar__item' target="_blank" href='https://flowcv.com/resume/gfw7e2wn1w'>resume</a></li>
-      </ul> */}
-
-
       <div
         className={`nav-bar__menu-line ${navActive ? "active" : ""
           }`}
         onClick={() => {
-          // setActiveIdx(idx);
           setNavActive(!navActive);
         }}
       >
@@ -36,12 +24,31 @@ const Nav = () => {
           }`}
       >
         <ul>
-          <li><Link className='nav-bar__item' href={'#about'}>about</Link></li>
-          <li><Link className='nav-bar__item' href={'#skills'}>skills</Link></li>
-          <li><Link className='nav-bar__item' href={'#projects'}>projects</Link></li>
-          <li><Link className='nav-bar__item' href={'#blogs'}>blogs</Link></li>
-          <li><Link className='nav-bar__item' href={'#contact'}>contact</Link></li>
-          <li><a className='nav-bar__item' target="_blank" href='https://flowcv.com/resume/gfw7e2wn1w'>resume</a></li>
+          <li><Link
+            className='nav-bar__item'
+            onClick={() => { setNavActive(false); }}
+            href={'#about'}
+          >about</Link></li>
+          <li><Link
+            className='nav-bar__item'
+            onClick={() => { setNavActive(false); }}
+            href={'#skills'}>skills</Link></li>
+          <li><Link
+            className='nav-bar__item'
+            onClick={() => { setNavActive(false); }}
+            href={'#projects'}>projects</Link></li>
+          <li><Link
+            className='nav-bar__item'
+            onClick={() => { setNavActive(false); }}
+            href={'#blogs'}>blogs</Link></li>
+          <li><Link
+            className='nav-bar__item'
+            onClick={() => { setNavActive(false); }}
+            href={'#contact'}>contact</Link></li>
+          <li><a
+            className='nav-bar__item'
+            onClick={() => { setNavActive(false); }}
+            target="_blank" href='https://flowcv.com/resume/gfw7e2wn1w'>resume</a></li>
         </ul>
       </div>
 
